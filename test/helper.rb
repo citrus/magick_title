88208d28ea5cadca0efb93a38f39e0a5911e287d
Spencer Steffen <spencer@citrusme.com>
2011-03-13 23:04:09 -0700

    updated demo app

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ class Test::Unit::TestCase
   def setup
     super
     MagickTitle.options[:root] = File.expand_path("../dummy", __FILE__)
-    FileUtils.rm_r MagickTitle.options.destination if Dir.exists?(MagickTitle.options.destination)
+    #FileUtils.rm_r MagickTitle.options.destination if Dir.exists?(MagickTitle.options.destination)
   end
 
 end
--- test/test_magick_title.rb
+++ test/test_magick_title.rb
@@ -11,4 +11,13 @@ class TestMagickTitle < Test::Unit::TestCase
     
   end
   
+  
+  
+  should "not get cut off" do
+    
+    @title = MagickTitle.say("HELLO MAGICK TITLE OF DOOM!", :font_path => "/Users/Spencer/Library/Fonts", :font => "HelveticaNeueLTStd-UltLt.otf", :color => "#000000")
+    assert File.exists?(@title.fullpath)
+    
+  end
+  
 end
